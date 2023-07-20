6461b5af192d2469049d2fffe074ca748b4c09c8
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2023-07-20 18:59:28 +0900

    re

--- LoginPanel.java
+++ LoginPanel.java
@@ -90,10 +90,10 @@ public class LoginPanel extends JPanel {
 		exitButton_ = new JButton("Exit");
 		exitButton_.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
 		exitButton_.setContentAreaFilled(false);
-		exitButton_.setBackground(Color.white);
 		exitButton_.setMargin(new Insets(0, 0, 0, 0));
-		exitButton_.setForeground(UIManager.getColor("Button.disabledText"));
 		exitButton_.setBorderPainted(true);
+		exitButton_.setForeground(UIManager.getColor("Button.disabledText"));
+		exitButton_.setBackground(Color.white);
 		exitButton_.setBounds(30, 30, 80, 30);
 		this.add(exitButton_);
 		
